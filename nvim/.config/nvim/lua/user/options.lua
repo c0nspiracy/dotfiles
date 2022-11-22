545f5dd7d4649a69b7aaef496e999bd8261ebe4e
Robert Newbould <robert.newbould@freeagent.com>
2022-11-22 17:25:01 +0000

    neovim: set listchars and showbreak options

--- nvim/.config/nvim/lua/user/options.lua
+++ nvim/.config/nvim/lua/user/options.lua
@@ -5,5 +5,5 @@ vim.opt.relativenumber = true
 vim.opt.smartcase = true
 
 vim.opt.list = true
-vim.opt.listchars:append "space:⋅"
-vim.opt.listchars:append "eol:↴"
+vim.opt.listchars = "space:⋅,tab:→ ,eol:↴,nbsp:␣,trail:•,extends:⟩,precedes:⟨"
+vim.opt.showbreak = "↪ "
