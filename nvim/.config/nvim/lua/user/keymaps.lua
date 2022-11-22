784db70d6b8925fcb033b42300555c9894948dfe
Robert Newbould <robert.newbould@freeagent.com>
2022-11-22 17:39:21 +0000

    neovim: add window navigation and explorer keymaps

--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -3,6 +3,24 @@ local opts = { noremap = true, silent = true }
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
 
+-- Modes
+--   "n" - normal_mode
+--   "i" - insert mode
+--   "v" - visual mode
+--   "x" - visual block mode
+--   "t" - term mode
+--   "c" - command mode
+
+-- Normal --
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Open explorer on the left-hand side
+keymap("n", "<Leader>e", ":Lex 30<CR>", opts)
+
 keymap("n", "<Leader>ff", ":Telescope find_files<CR>", opts)
 keymap("n", "<Leader>fg", ":Telescope live_grep<CR>", opts)
 keymap("n", "<Leader>fG", ":Telescope grep_string<CR>", opts)
