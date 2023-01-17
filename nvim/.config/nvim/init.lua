7a59dd4bfe0d54b66c0fe4d1fdad8c111e62ef7b
Robert Newbould <robert.newbould@freeagent.com>
2023-01-17 13:00:19 +0000

    neovim: set formatting options for lua files

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -17,3 +17,4 @@ try_require('plugins.neotest')
 try_require('plugins.treesj')
 
 vim.cmd("autocmd Filetype javascript setlocal ts=2 sw=2 sts=2 expandtab")
+vim.cmd("autocmd Filetype lua setlocal ts=2 sw=2 sts=2 expandtab")
