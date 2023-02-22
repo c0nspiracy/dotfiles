913dedc433903eb76f47439a4a628d77743faf5e
Robert Newbould <robert.newbould@gmail.com>
2023-02-22 14:59:37 +0000

    neovim: add additional keymaps for neotest

--- nvim/.config/nvim/after/plugin/neotest.lua
+++ nvim/.config/nvim/after/plugin/neotest.lua
@@ -14,5 +14,9 @@ neotest.setup({
 
 vim.keymap.set('n', '<leader>kn', function() neotest.run.run() end, opts)
 vim.keymap.set('n', '<leader>kf', function() neotest.run.run(vim.fn.expand("%")) end, opts)
-vim.keymap.set('n', '<leader>ko', function() neotest.output.open() end, opts)
+vim.keymap.set('n', '<leader>ka', function() neotest.run.attach() end, opts)
 
+vim.keymap.set('n', '<leader>ko', function() neotest.output.open() end, opts)
+vim.keymap.set('n', '<leader>kO', function() neotest.output.open({ enter = true }) end, opts)
+
+vim.keymap.set('n', '<leader>ks', function() neotest.summary.toggle() end, opts)
