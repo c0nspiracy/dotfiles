54472a18e03d61086b044cc05ca4340bdb1d1645
Robert Newbould <robert.newbould@gmail.com>
2023-02-22 15:23:01 +0000

    neovim: [LSP] override rubocop output handler to fix off-by-one column bug?

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -39,6 +39,60 @@ local null_ls = require("null-ls")
 local h = require("null-ls.helpers")
 local null_opts = lsp.build_options("null-ls", {})
 
+local offense_to_diagnostic = function(offense)
+  local diagnostic = nil
+
+  diagnostic = {
+    message = offense.message,
+    ruleId = offense.cop_name,
+    level = offense.severity,
+    line = offense.location.start_line,
+    column = offense.location.start_column,
+    endLine = offense.location.last_line,
+    endColumn = offense.location.last_column,
+  }
+
+  if offense.location.start_line ~= offense.location.last_line then
+    diagnostic = vim.tbl_extend("force", diagnostic, { endLine = offense.location.start_line, endColumn = 0 })
+  end
+
+  if offense.location.start_column == offense.location.last_column then
+    diagnostic = vim.tbl_extend("force", diagnostic, { column = 0, endColumn = 0 })
+  end
+
+  return diagnostic
+end
+
+local handle_rubocop_output = function(params)
+  if params.output and params.output.files then
+    local file = params.output.files[1]
+    if file and file.offenses then
+      local parser = h.diagnostics.from_json({
+        severities = {
+          info = h.diagnostics.severities.information,
+          convention = h.diagnostics.severities.information,
+          refactor = h.diagnostics.severities.hint,
+          warning = h.diagnostics.severities.warning,
+          error = h.diagnostics.severities.error,
+          fatal = h.diagnostics.severities.fatal,
+        },
+        offsets = {
+          end_col = 1,
+        },
+      })
+      local offenses = {}
+
+      for _, offense in ipairs(file.offenses) do
+        table.insert(offenses, offense_to_diagnostic(offense))
+      end
+
+      return parser({ output = offenses })
+    end
+  end
+
+  return {}
+end
+
 null_ls.setup({
   debug = true,
   on_attach = null_opts.on_attach,
@@ -47,6 +101,7 @@ null_ls.setup({
       command = "docker-rubocop",
       args = { "-f", "json", "--force-exclusion", "--stdin", "$FILENAME" },
       to_stdin = true,
+      on_output = handle_rubocop_output,
     })
   }
 })
