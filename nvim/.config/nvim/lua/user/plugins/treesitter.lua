5a75d75ad061699b20c6bef6eda008b40e0e560e
Robert Newbould <robert.newbould@gmail.com>
2023-11-01 22:45:15 +0000

    [neovim] move treesitter config inside its plugin definition file

--- nvim/.config/nvim/lua/user/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/user/plugins/treesitter.lua
@@ -1,5 +1,64 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   "nvim-treesitter/nvim-treesitter-textobjects",
-  "nvim-treesitter/playground"
+  "nvim-treesitter/playground",
+  build = ":TSUpdate",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local treesitter = require("nvim-treesitter.configs")
+
+    vim.opt.foldmethod = expr
+    vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+
+    treesitter.setup({
+      ensure_installed = {
+        "ruby",
+        "html",
+        "css",
+        "javascript",
+        "lua",
+        "rust",
+      },
+      context_commentstring = {
+        enable = true
+      },
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      indent = {
+        enable = true,
+        disable = { "ruby" }
+      },
+      matchup = {
+        enable = true
+      },
+      textobjects = {
+        select = {
+          enable = true,
+
+          -- Automatically jump forward to textobj, similar to targets.vim
+          lookahead = true,
+
+          keymaps = {
+            -- You can use the capture groups defined in textobjects.scm
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+          },
+        },
+        swap = {
+          enable = true,
+
+          swap_next = {
+            ["<leader>."] = "@parameter.inner",
+          },
+          swap_previous = {
+            ["<leader>,"] = "@parameter.inner",
+          },
+        },
+      },
+    })
+  end
 }
