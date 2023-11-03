f3666e968b5932e3f6a02ea8af8cd7c0c100a99e
Robert Newbould <robert.newbould@gmail.com>
2023-11-03 10:54:35 +0000

    [neovim] configure lazy to not notify about package updates or changed files

--- nvim/.config/nvim/lua/user/lazy.lua
+++ nvim/.config/nvim/lua/user/lazy.lua
@@ -11,5 +11,16 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup('user.plugins')
+require("lazy").setup("user.plugins", {
+  install = {
+    colorscheme = { "dracula" },
+  },
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
+})
 -- vim: ts=2 sts=2 sw=2 et
