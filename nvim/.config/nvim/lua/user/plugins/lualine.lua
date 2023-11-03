7927cb68d4be6109b6b91c083ca5a65724f30cd1
Robert Newbould <robert.newbould@gmail.com>
2023-11-03 10:51:24 +0000

    [neovim] configure lualine to indicate when packages have updates available

--- nvim/.config/nvim/lua/user/plugins/lualine.lua
+++ nvim/.config/nvim/lua/user/plugins/lualine.lua
@@ -3,7 +3,18 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = "nvim-web-devicons",
     opts = {
-      options = { theme = "dracula" }
+      options = {
+        theme = "dracula",
+      },
+      sections = {
+        lualine_x = {
+          {
+            require("lazy.status").updates,
+            cond = require("lazy.status").has_updates,
+            color = { fg = "#ff9e64" },
+          },
+        },
+      }
     }
   }
 }
