c61b3578556cd64ad3b5fef03df2b9377a0c9aa7
Robert Newbould <robert.newbould@gmail.com>
2023-11-23 12:17:15 +0000

    [neovim] extract which-key plugin definition and config

--- nvim/.config/nvim/lua/user/plugins/init.lua
+++ nvim/.config/nvim/lua/user/plugins/init.lua
@@ -7,18 +7,6 @@ return {
   -- defaults
   "tpope/vim-sensible",
 
-  -- which-key
-  {
-    "folke/which-key.nvim",
-    config = function()
-      require("which-key").setup {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      }
-    end
-  },
-
   -- file navigation
   "theprimeagen/harpoon",
 
