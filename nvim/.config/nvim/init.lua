6876b4e064cce532f3974dffb87f64ea3aea5de6
Robert Newbould <robert.newbould@gmail.com>
2023-11-23 12:39:22 +0000

    [neovim] add user commands for commonly miscapitalised commands, eg. :W, :Q

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -15,3 +15,9 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     })
   end,
 })
+
+vim.api.nvim_create_user_command('WQ', 'wq', {})
+vim.api.nvim_create_user_command('Wq', 'wq', {})
+vim.api.nvim_create_user_command('W', 'w', {})
+vim.api.nvim_create_user_command('Qa', 'qa', {})
+vim.api.nvim_create_user_command('Q', 'q', {})
