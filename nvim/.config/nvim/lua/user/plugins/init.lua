269780af9bd84193f8ccf8e7e040a825353fb4f9
Robert Newbould <robert.newbould@gmail.com>
2023-11-23 20:55:50 +0000

    [neovim] extract barbecue plugin definition and config

--- nvim/.config/nvim/lua/user/plugins/init.lua
+++ nvim/.config/nvim/lua/user/plugins/init.lua
@@ -45,19 +45,6 @@ return {
   "eandrju/cellular-automaton.nvim",
 
   {
-    "utilyre/barbecue.nvim",
-    name = "barbecue",
-    version = "*",
-    dependencies = {
-      "SmiteshP/nvim-navic",
-      "nvim-tree/nvim-web-devicons", -- optional dependency
-    },
-    opts = {
-      -- configurations go here
-    },
-  },
-
-  {
     'mrjones2014/dash.nvim',
     build = 'make install',
   },
