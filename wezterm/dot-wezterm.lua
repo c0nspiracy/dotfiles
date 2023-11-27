3ad2eafd611f71ea0ba53a51e2b1fd048278d420
Robert Newbould <robert.newbould@gmail.com>
2023-11-27 16:52:22 +0000

    [wezterm] add initial configuration

---
+++ wezterm/dot-wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require("wezterm")
+
+return {
+  font = wezterm.font("VictorMono Nerd Font"),
+  font_size = 18.0,
+  color_scheme = "Catppuccin Macchiato",
+  enable_tab_bar = false,
+  send_composed_key_when_left_alt_is_pressed = true,
+}
