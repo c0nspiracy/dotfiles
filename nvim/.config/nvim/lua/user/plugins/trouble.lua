f7c0004669e392fdfbbeb5810fbb541488959099
Robert Newbould <robert.newbould@gmail.com>
2023-11-27 11:27:23 +0000

    [neovim] trouble: add keybindings for toggling and navigating through telescope search results

--- nvim/.config/nvim/lua/user/plugins/trouble.lua
+++ nvim/.config/nvim/lua/user/plugins/trouble.lua
@@ -10,6 +10,10 @@ return {
     vim.keymap.set("n", "<leader>xd", function() trouble.toggle("document_diagnostics") end)
     vim.keymap.set("n", "<leader>xq", function() trouble.toggle("quickfix") end)
     vim.keymap.set("n", "<leader>xl", function() trouble.toggle("loclist") end)
+    vim.keymap.set("n", "<leader>xt", function() trouble.toggle("telescope") end)
     vim.keymap.set("n", "gR", function() trouble.toggle("lsp_references") end)
+
+    vim.keymap.set("n", "[t", function() trouble.previous({ skip_groups = true, jump = true }) end)
+    vim.keymap.set("n", "]t", function() trouble.next({ skip_groups = true, jump = true }) end)
   end,
 }
