0580f811ee880772c1eeb026b085f1658e4c6513
Robert Newbould <robert.newbould@gmail.com>
2023-11-28 12:45:31 +0000

    [wezterm] show tab bar if more than one tab

--- wezterm/dot-wezterm.lua
+++ wezterm/dot-wezterm.lua
@@ -4,6 +4,7 @@ return {
   font = wezterm.font("VictorMono Nerd Font", { weight = "Medium" }),
   font_size = 18.0,
   color_scheme = "Catppuccin Macchiato",
-  enable_tab_bar = false,
+  enable_tab_bar = true,
+  hide_tab_bar_if_only_one_tab = true,
   send_composed_key_when_left_alt_is_pressed = true,
 }
