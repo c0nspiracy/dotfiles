3c5c8f8b2c9f2e277b141514010c32027d6c72af
Robert Newbould <robert.newbould@gmail.com>
2025-01-03 16:53:49 +0000

    [neovim] update config for lualine

--- nvim/dot-config/nvim/lua/user/plugins/lualine.lua
+++ nvim/dot-config/nvim/lua/user/plugins/lualine.lua
@@ -1,8 +1,21 @@
 return {
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = "nvim-web-devicons",
-    opts = {
+  "nvim-lualine/lualine.nvim",
+  dependencies = "nvim-web-devicons",
+
+  opts = function(_, opts)
+    local trouble = require("trouble")
+    local symbols = trouble.statusline({
+      mode = "lsp_document_symbols",
+      groups = {},
+      title = false,
+      filter = { range = true },
+      format = "{kind_icon}{symbol.name:Normal}",
+      -- The following line is needed to fix the background color
+      -- Set it to the lualine section you want to use
+      hl_group = "lualine_c_normal",
+    })
+
+    table.insert(opts, {
       options = {
         theme = "catppuccin-macchiato",
       },
@@ -17,7 +30,13 @@ return {
           { "fileformat" },
           { "filetype" },
         },
+        lualine_c = {
+          {
+            symbols.get,
+            cond = symbols.has,
+          },
+        },
       }
-    }
-  }
+    })
+  end
 }
