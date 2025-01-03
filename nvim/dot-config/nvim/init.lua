d09be84b430742442bcb0e2337c9063e7aff9dff
Robert Newbould <robert.newbould@gmail.com>
2025-01-03 16:50:53 +0000

    [neovim] disable autoindent when typing "." in ruby files

--- nvim/dot-config/nvim/init.lua
+++ nvim/dot-config/nvim/init.lua
@@ -5,6 +5,7 @@ vim.g.loaded_matchit = 1
 
 vim.cmd("autocmd Filetype javascript setlocal ts=2 sw=2 sts=2 expandtab")
 vim.cmd("autocmd Filetype lua setlocal ts=2 sw=2 sts=2 expandtab")
+vim.cmd("autocmd Filetype ruby setlocal indentkeys-=.")
 
 local yank_group = vim.api.nvim_create_augroup("HighlightYank", {})
 
